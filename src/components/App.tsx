cb50f3fa1434292ecae068ce58cdaffeeba233bc
TsilaAllaoui <tsila.allaoui@gmail.com>
2023-10-04 13:44:51 +0300

    Better login page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./SignUp";
-import Login from './Login'
-import '../styles/App.scss'
+import Login from "./Login";
+import "../styles/App.scss";
 import Main from "./Main";
 
 // Sender and Receiver types
 type Receiver = {
-  name: string,
-  id: string
+  name: string;
+  id: string;
 };
 type Sender = Receiver;
 
-
 function App() {
-
   // Hardcoded value for debugging
   const receiverName: string = "Tsila";
   const receiverId: string = "cKRWwlRPOfPaWdtv58SB";
@@ -21,17 +19,15 @@ function App() {
   const senderId = "NeLZhBOa04SkiEVcAEPf";
 
   return (
-    <div>
-      <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/main' element={<Main/>} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<SignUp />} />
-          </Routes>
-      </BrowserRouter>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,11 +6,11 @@ import {
   signInWithEmailAndPassword,
   signInWithPopup,
 } from "@firebase/auth";
-import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
-import logo from "../assets/logo.svg";
+import { Link, useNavigate } from "react-router-dom";
 import app, { auth, gauthProvider } from "../Firebase";
+import logo from "../assets/logo.svg";
 import "../styles/Login.scss";
 
 function Login() {
@@ -19,6 +19,7 @@ function Login() {
   // States for the inputs
   const [emailValue, setEmailValue] = useState("ratsilakwel@gmail.com");
   const [passwordValue, setPasswordValue] = useState("123456789");
+  const [agreementsChecked, setAgreementsChecked] = useState(false);
 
   // ************** Hooks ***************
 
@@ -34,8 +35,24 @@ function Login() {
 
   // ************** Functions ****************
 
+  const setAgreementsError = () => {
+    const agreements = document.querySelector(
+      "#agreements > p"
+    ) as HTMLPreElement;
+    agreements.style.color = "red";
+    agreements.style.animation = "shake 500ms ease-in-out";
+    setTimeout(() => {
+      agreements.style.color = "black";
+      agreements.style.animation = "";
+    }, 500);
+  };
+
   // For login redirection
   const redirectToLogin = () => {
+    if (!agreementsChecked) {
+      setAgreementsError();
+      return;
+    }
     const auth = getAuth(app);
     setPersistence(auth, browserSessionPersistence).then(() =>
       signInWithEmailAndPassword(auth, emailValue, passwordValue)
@@ -47,6 +64,10 @@ function Login() {
   };
 
   const loginWithGoolge = () => {
+    if (!agreementsChecked) {
+      setAgreementsError();
+      return;
+    }
     setPersistence(auth, browserSessionPersistence).then(() => {
       signInWithPopup(auth, gauthProvider)
         .then((result) => {
@@ -67,6 +88,14 @@ function Login() {
           );
         });
     });
+  };
+
+  const clickAgreements = (e: React.MouseEvent<HTMLElement>) => {
+    const checkBox = document.querySelector(
+      "#agreements > input"
+    ) as HTMLInputElement;
+    checkBox.checked = agreementsChecked ? false : true;
+    setAgreementsChecked(checkBox.checked);
   };
 
   // ****************** Rendering *****************
@@ -109,8 +138,12 @@ function Login() {
           />
         </div>
         <div id="agreements">
-          <input type="checkbox" />
-          <p>Accept all agreements</p>
+          <input
+            type="checkbox"
+            checked={agreementsChecked}
+            onChange={(e) => setAgreementsChecked(e.currentTarget.checked)}
+          />
+          <p onClick={clickAgreements}>Accept all agreements</p>
         </div>
         <div id="buttons">
           <button id="forgot-password-button">Forgot password</button>
@@ -121,8 +154,6 @@ function Login() {
         <p>Or login with: </p>
         <div id="social-login">
           <FcGoogle id="google" onClick={loginWithGoolge} />
-          {/* <AiOutlineMail id="mail" />
-          <AiOutlineFacebook id="fb" /> */}
         </div>
       </div>
     </div>
